45ab37b21d1b1e1849189e7fb951b3e3ec7c95a4
taiht <Hdt13102k3@gmail.com>
2024-08-19 08:49:56 +0700

    Relationship of Tour, Location, TourLocation

--- relationship/associations.js
+++ relationship/associations.js
@@ -1,5 +1,17 @@
 import Tour from "../models/Tour.js";
 import TourChild from '../models/TourChild.js';
+import TourLocation from "../models/TourLocation.js";
+import Location from "../models/Location.js";
 
+
+// Relationship one to many between Tour and TourChild
 Tour.hasMany(TourChild, { foreignKey: 'tour_id', as: 'tourChildren' });
 TourChild.belongsTo(Tour, { foreignKey: 'tour_id', as: 'tour' });
+
+
+// The relationship of the three tables Tour, Location and TourLoation
+Tour.hasMany(TourLocation, { foreignKey: 'tour_id', as: 'tourLocations' });
+Location.hasMany(TourLocation, { foreignKey: 'location_id', as: 'tourLocations' });
+TourLocation.belongsTo(Tour, { foreignKey: 'tour_id', as: 'tour' });
+TourLocation.belongsTo(Location, { foreignKey: 'location_id', as: 'location' });
+
