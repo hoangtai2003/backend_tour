98a801891c348591d3b57a8bc7d26e9b855e1084
taiht <Hdt13102k3@gmail.com>
2024-08-21 02:15:31 +0700

    handle image ckeditor

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import cors from 'cors'
 import cookieParser from 'cookie-parser'
 import sequelize from './sequelize.js';
 import './relationship/associations.js';
+import multiparty from 'connect-multiparty'
 
 import tourRoute from './routes/tours.js'
 import userRoute from './routes/users.js'
@@ -22,6 +23,7 @@ const corsOption = {
     origin: true, // Cho phép mọi miền truy cập.
     credentials: true // Cho phép gửi thông tin xác thực (cookie, HTTP Authentication) cùng với yêu cầu CORS.
 }
+const MultipartyMiddleware = multiparty({uploadDir: './images/tours'});
 //database connection
 sequelize.authenticate()
     .then(() => console.log('MySQL database connected'))
@@ -32,6 +34,7 @@ sequelize.authenticate()
 app.use(express.json()) // nếu một yêu cầu POST hoặc PUT gửi dữ liệu JSON, express.json() sẽ phân tích dữ liệu đó và đưa vào req.body để bạn có thể dễ dàng truy cập nó.
 app.use(cors(corsOption)) //Quản lý các yêu cầu từ nguồn gốc khác (CORS).
 app.use(cookieParser()) // Phân tích cookie và đưa chúng vào req.cookies
+app.use('/images/tours', express.static('images/tours'));
 
 app.use('/api/v1/auth', authRoute)
 app.use('/api/v1/tours', tourRoute)
@@ -40,7 +43,34 @@ app.use('/api/v1/review', reviewRoute)
 app.use('/api/v1/booking', bookingRoute)
 app.use('/api/v1/location', locationRoute)
 
+app.post('/uploads', MultipartyMiddleware, (req, res) => {
+    console.log(req.files); // Log files received
+    if (!req.files || !req.files.upload) {
+        return res.status(400).json({
+            uploaded: false,
+            error: {
+                message: 'No files were uploaded.'
+            }
+        });
+    }
 
+    const file = Array.isArray(req.files.upload) ? req.files.upload[0] : req.files.upload;
+    if (!file || !file.path) {
+        return res.status(400).json({
+            uploaded: false,
+            error: {
+                message: 'File path is not defined.'
+            }
+        });
+    }
+
+    const filePath = `${req.protocol}://${req.get('host')}/images/tours/${file.path.split('\\').pop()}`;
+
+    res.status(200).json({
+        uploaded: true,
+        url: filePath
+    });
+});
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
