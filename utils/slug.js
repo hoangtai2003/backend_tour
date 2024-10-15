094a4fefdc5c16a0fcd32ffa05fcff964cf2a1d1
taiht <Hdt13102k3@gmail.com>
2024-10-15 10:21:53 +0700

    edit slug

--- utils/slug.js
+++ utils/slug.js
@@ -2,8 +2,9 @@ export const createSlug = (news_name) => {
     return news_name
         .toLowerCase() // Chuyển chữ thành chữ thường
         .normalize('NFD').replace(/[\u0300-\u036f]/g, '') // Loại bỏ dấu tiếng Việt
+        .replace(/[Đđ]/g, 'd') // Chuyển Đ/đ thành d
         .replace(/[^\w\s-]/g, '') // Loại bỏ ký tự đặc biệt
         .trim() // Loại bỏ khoảng trắng ở đầu và cuối
         .replace(/\s+/g, '-') // Thay thế khoảng trắng bằng dấu gạch ngang
         .replace(/-+/g, '-'); // Loại bỏ các dấu gạch ngang dư thừa
-}
+};
