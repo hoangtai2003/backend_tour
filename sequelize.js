19baabbcce03136f962793bc44e2e9d37447b23d
taiht <Hdt13102k3@gmail.com>
2024-11-18 22:42:09 +0700

    library

--- sequelize.js
+++ sequelize.js
@@ -5,7 +5,6 @@ dotenv.config();
 const sequelize = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {
     host: process.env.MYSQL_HOST,
     dialect: 'mysql',
-    port: 3306, 
     logging: false,
 });
 
